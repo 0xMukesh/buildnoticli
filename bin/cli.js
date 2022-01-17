a46a8fc23c764e0b7d49e675531b6b2fcecd63a4
Kira <mukeshreddy.9.new@gmail.com>
2022-01-17 15:08:11 +0530

    fix(bin/index.js): fix a minor bug

--- bin/cli.js
+++ bin/cli.js
@@ -11,16 +11,27 @@ import questions from './utils/questions.js'
 const __dirname = path.resolve(path.dirname(''));
 
 inquirer.prompt(questions).then((answers) => {
-    const commandArray = answers.command.split(',')
-    commandArray.map((command) => {
-        const runningCommand = shell.exec(command)
+    if (answers.command.includes(",")) {
+        const commandArray = answers.command.split(',')
+        commandArray.map((command) => {
+            const runningCommand = shell.exec(command)
+            if (runningCommand.code !== 0) {
+                console.log(chalk.red(chalk.bold("ERROR!")), `The ${chalk.bold(command)} command had an error while executing`)
+            }
+            else {
+                console.log(chalk.green(chalk.bold("SUCCESS!"), `The ${chalk.bold(command)} command had been successfully completed`))
+            }
+        })
+    }
+    else {
+        const runningCommand = shell.exec(answers.command)
         if (runningCommand.code !== 0) {
-            console.log(chalk.red(chalk.bold("ERROR!")), `The ${chalk.bold(command)} command had an error while executing`)
+            console.log(chalk.red(chalk.bold("ERROR!")), `The ${answers.command} command had an error while executing`)
         }
         else {
-            console.log(chalk.green(chalk.bold("SUCCESS!"), `The ${chalk.bold(command)} command had been successfully completed`))
+            console.log(chalk.green(chalk.bold("SUCCESS!"), `The ${answers.command} command had been successfully completed`))
         }
-    })
+    }
     notifier.notify({
         title: `${answers.message}`,
         message: 'Hey dev 👋, I have completed running the build task. Please have a look 😄',
